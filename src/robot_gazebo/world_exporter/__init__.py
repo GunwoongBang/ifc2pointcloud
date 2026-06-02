c558f0b31dbb2ac5ca17cb9b9e5ac6f16f96f1d0
Gunwoong Bang <gw.bang@tum.de>
2026-06-02 19:43:09 +0200

    repo merged to bim3dscenegraph - now ifc2pointcloud is not cannot be run solely but only within bim3dscenegraph

--- src/robot_gazebo/world_exporter/__init__.py
+++
@@ -1,9 +0,0 @@
-from geometry import extract_mesh_from_shape
-from sdf_util import safe_name, write_obj, pretty_xml
-
-__all__ = [
-    "extract_mesh_from_shape",
-    "safe_name",
-    "write_obj",
-    "pretty_xml",
-]
